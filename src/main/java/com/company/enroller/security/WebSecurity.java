6016d8c13860ea768bb208e394ec4442ec769468
lonegunman82 <mateusz.pietrzyk@gmail.com>
2024-06-09 22:47:29 +0200

    Dodane logowanie. Nieprzetestowane.

--- src/main/java/com/company/enroller/security/WebSecurity.java
+++ src/main/java/com/company/enroller/security/WebSecurity.java
@@ -1,11 +1,16 @@
+package com.company.enroller.security;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import com.company.enroller.security.ParticipantProvider;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -16,6 +21,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    private final String secret = "yourSecretKey";
+    private final String issuer = "yourIssuer";
+    private final int tokenExpiration = 86400; // 1 day
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(participantProvider).passwordEncoder(passwordEncoder);
@@ -23,14 +32,23 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // Tutaj możesz dodać konfigurację zabezpieczeń HTTP
-        // Na przykład:
-        http.authorizeRequests()
+        JWTAuthenticationFilter jwtAuthenticationFilter = new JWTAuthenticationFilter(authenticationManagerBean(), secret, issuer, tokenExpiration);
+
+        http.csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/public/**").permitAll()
+                .antMatchers("/tokens").permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin().permitAll()
                 .and()
                 .logout().permitAll();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
